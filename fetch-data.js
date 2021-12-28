7529850e09e5ef54c84a6d2e92bfa8ac352556da
MCodrescu <m.codrescu@outlook.com>
2021-12-27 22:40:31 -0600

    Data API

---
+++ fetch-data.js
@@ -0,0 +1,16 @@
+// Set the options for the API call
+const myInit = {
+  method: "GET",
+  mode: "same-origin",
+};
+
+// Create a new request to the API URL
+let myRequest = new Request("http://localhost:8080/data");
+
+// Fill the data object with the result from the API call
+
+fetch(myRequest, myInit).then((response) => {
+  response.json().then((data) => {
+    console.log(data);
+  });
+});
--- plumber.R
+++ plumber.R
@@ -1,9 +1,15 @@
-library(plumber)
-
 #* @apiTitle Index Page
 
 #* Serve the index page
 #* @get /
 function(req, res){
   include_html("index.html", res = res)
+}
+
+#* @apiTitle Data
+
+#* Serve the data
+#* @get /data
+function(req, res){
+  sample(1:10)
 }
